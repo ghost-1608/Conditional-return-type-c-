a302303148e29508e08f277378f52f726343e041
ghost1608 <64543976+ghost-1608@users.noreply.github.com>
2022-05-12 00:33:59 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,42 @@
+/* Program implementing a function returning either int or string based on the input argument */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Declaring struct containing enum storing type of the return value, and void pointer to store the return value
+struct ret
+{
+	enum {I, S} type;
+	void* ret;
+};
+
+// Function of the type struct ret to return the type of the return value along with the return value
+struct ret foo (char i)
+{
+	struct ret r;
+
+	if (i == 's')
+	{
+		r.type = S;
+		r.ret = malloc(6);
+		sprintf((char*)r.ret, "hello");
+	}
+	else if (i == 'i')
+	{
+		r.type = I;
+		r.ret = malloc(4);
+		*((int*)r.ret) = 69;
+	}
+
+	return r;
+}
+
+int main()
+{
+	struct ret r = foo('i');
+
+	if (r.type == S)
+		printf("%s", (char*)r.ret);
+	else if (r.type == I)
+		printf("%d", *((int*)r.ret));
+}
---
+++ test.py
@@ -0,0 +1,7 @@
+def foo(i):
+	if i == 's':
+		return 'hello'
+	elif i == 'i':
+		return 69
+
+print(foo('s'))
